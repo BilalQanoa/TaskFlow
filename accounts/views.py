876a55cab67f28a087dfa7d177de925ab03c3ab9
BilalQanoa <bilalqanoa@gmail.com>
2026-06-17 10:48:24 +0300

    prepare accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register(request):
+    return render (request, 'register.html')
+
+
+def login (request):
+    return render (request, 'login.html')
+
+
+def profile (request):
+    return render (request, 'profile.html')
+
+def logout (request):
+    pass
+
